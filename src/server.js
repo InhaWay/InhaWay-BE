868463d73aeaa506ad51491f2fce7d54c0b21f8e
huniversal <hunivers0523@naver.com>
2025-10-01 00:02:32 +0900

    Fix: buildingRoutes import 위치 변경

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
-const buildingRoutes = require('./routes/buildingRoutes');
 
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-// mongoDB는 나중에 연결 할 예정
 const cors = require('cors');
+const buildingRoutes = require('./routes/buildingRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,7 +16,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// MongoDB 연결 (나중에 활성화)
+// MongoDB 연결 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB 연결 성공'))
   .catch((err) => console.error('MongoDB 연결 실패:', err));
